80121e15cd51b9bbb9ea13dcc7c992c266ab3893
Sultan Zacky Hermawan <sultanzacky318@gmail.com>
2026-06-01 15:13:03 +0700

    update

--- src/pages/RegisterPage.vue
+++ src/pages/RegisterPage.vue
@@ -49,7 +49,7 @@ const handleRegister = async () => {
   
       <form @submit.prevent="handleRegister" class="space-y-5">
         <div>
-          <label class="block mb-1.5 text-sm font-medium text-gray-700">Username / Email</label>
+          <label class="block mb-1.5 text-sm font-medium text-gray-700">Username</label>
           <input v-model="username" type="text" placeholder="Buat nama pengguna atau email" required
             class="w-full px-4 py-2.5 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition" />
         </div>
